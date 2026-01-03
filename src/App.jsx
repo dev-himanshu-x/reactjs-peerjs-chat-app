9be2bb3a458a00391f745ddcc53218054906d883
Himanshu <iam.himanshu.x@gmail.com>
2026-01-03 17:48:00 +0530

    reactjs and peerjs chat app completed

---
+++ src/App.jsx
@@ -0,0 +1,169 @@
+import { useState, useEffect, useRef } from "react";
+import Peer from "peerjs";
+import "./App.css";
+import MyComponent from "./Dashboard";
+import MyComponents from "./Dash";
+
+function App() {
+  const [peerId, setPeerId] = useState("");
+  const [remotePeerIdValue, setRemotePeerIdValue] = useState("");
+  const [message, setMessage] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [connection, setConnection] = useState(null);
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [dash, setDash] = useState(false);
+  const peerRef = useRef(null);
+  const connRef = useRef(null);
+  const newTime = new Date().toLocaleTimeString();
+  useEffect(() => {
+    const peer = new Peer();
+    peer.on("open", (id) => {
+      setPeerId(id);
+    });
+    peer.on("connection", (conn) => {
+      setConnection("Online");
+      connRef.current = conn;
+      conn.on("data", (data) => {
+        setTime(newTime);
+        setMessages((prev) => [...prev, { msg: data, time: time }]);
+      });
+    });
+    setConnection("Offline");
+    peerRef.current = peer;
+    return () => peer.destroy();
+  }, []);
+  function connectPeer() {
+    if (!remotePeerIdValue) return setConnection("Offline");
+    setConnection("Online");
+    const conn = peerRef.current.connect(remotePeerIdValue);
+    connRef.current = conn;
+    conn.on("open", function () {
+      conn.on("data", function (data) {
+        setTime(newTime);
+        setMessages((prev) => [...prev, { msg: data, time: time }]);
+      });
+    });
+  }
+  function sendMessage() {
+    connRef.current.send(message);
+    setTime(newTime);
+    setMessages((prev) => [...prev, { msg: message, time: time, type: "" }]);
+  }
+  function open() {
+    setDash(!dash);
+  }
+  return (
+    <div className="h-screen flex bg-[#EDEDED] border-t">
+      <MyComponent />
+      {dash && <MyComponents/>}
+      <div className="flex-1 flex flex-col">
+        <div className="h-16 bg-[#EDEDED] flex items-center px-2 border-b border-gray-200 justify-between ">
+          <button className=" mr-3 lg:hidden z-1" onClick={open}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#434343"
+            >
+              <path d="M120-120v-80h720v80H120Zm0-320v-80h720v80H120Zm0-320v-80h720v80H120Z" />
+            </svg>
+          </button>
+          <div className="flex items-center">
+            <img
+              src="public/image.png"
+              className="w-10 h-10 rounded-full mr-3 object-cover"
+            />
+            <div className="h-14 bg-[#EDEDED] flex items-center gap-2 justify-between flex-1">
+              <div>
+                <input
+                  value={remotePeerIdValue}
+                  onChange={(e) => setRemotePeerIdValue(e.target.value)}
+                  placeholder="Friends Peer Id"
+                  className="p-1 outline-none rounded-md bg-white text-base flex-1"
+                />
+                <div
+                  className={`text-xs mt-1 ${
+                    connection == "Online" ? "text-green-600" : "text-gray-600"
+                  }`}
+                >
+                  {connection}
+                </div>
+              </div>
+              <button onClick={connectPeer}>
+                <img src="public/send.png" className="h-5 w-5 mb-5" />
+              </button>
+            </div>
+          </div>
+          <div className="flex gap-2 mr-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#999888"
+            >
+              <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#999888"
+            >
+              <path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
+            </svg>
+          </div>
+        </div>
+        <div className="flex-1 bg-[#DCD9D0] p-2">
+          <div className="flex justify-center mt-2">
+            <div className="bg-[#f8f3d1] py-1 px-3 text-sm flex gap-1 items-center rounded-sm">
+              <img src="public/lock.png" className="h-3.5 w-3.5" />
+              Your Peer Id : {peerId}
+            </div>
+          </div>
+          {messages.map((msg, i) => (
+            <div
+              key={i}
+              className={`mt-3 flex ${
+                msg.type === "" ? "justify-end" : "justify-start"
+              }`}
+            >
+              <div
+                className={`flex w-fit gap-9 p-1.5 px-2.5 rounded-sm items-center ${
+                  msg.type === "" ? "bg-[#DBF8C6]" : "bg-white"
+                }`}
+              >
+                <div className="text-sm">{msg.msg}</div>
+                <div className="text-xs text-gray-500 mt-1.5">{msg.time}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="h-14 mb-2 bg-[#EDEDED] flex items-center px-4 gap-2 justify-between">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="rotate-50 scale-x-[-1]"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="gray"
+          >
+            <path d="M720-330q0 104-73 177T470-80q-104 0-177-73t-73-177v-370q0-75 52.5-127.5T400-880q75 0 127.5 52.5T580-700v350q0 46-32 78t-78 32q-46 0-78-32t-32-78v-370h80v370q0 13 8.5 21.5T470-320q13 0 21.5-8.5T500-350v-350q-1-42-29.5-71T400-800q-42 0-71 29t-29 71v370q-1 71 49 120.5T470-160q70 0 119-49.5T640-330v-390h80v390Z" />
+          </svg>
+          <input
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Type a message here..."
+            className=" px-4 py-2 outline-none rounded-md flex-1 bg-white text-base"
+          />
+          <button onClick={sendMessage}>
+            <img src="public/send.png" className="h-5 w-5" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ src/Dash.jsx
@@ -0,0 +1,252 @@
+import { useState } from "react";
+function MyComponents() {
+  return (
+    <div className={`border-r border-gray-300 block lg:hidden fixed bg-[#EDEDED]`}>
+      <div className="flex items-center p-3 justify-between mr-6">
+        <div>
+          <img
+            src="public/person.png"
+            className="w-10 h-10 rounded-full object-cover"
+          />
+        </div>
+        <div className="flex items-center gap-4">
+          <img
+            src="public/moon.png"
+            className="w-7 h-7 rounded-full object-cover mr-2"
+          />
+          <div>
+            <svg
+              className="mr-3"
+              fill="#878787"
+              width="24px"
+              height="24px"
+              viewBox="0 0 256.00 256.00"
+              id="Flat"
+              xmlns="http://www.w3.org/2000/svg"
+              stroke="#878787"
+              stroke-width="0.00256"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke="#CCCCCC"
+                stroke-width="2.048"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <path d="M65.77441,54.46033a8.00119,8.00119,0,0,1,0,11.31445,87.95677,87.95677,0,0,0-22.78955,39.4375,7.99995,7.99995,0,1,1-15.45849-4.127,103.97686,103.97686,0,0,1,26.93457-46.625A8.00178,8.00178,0,0,1,65.77441,54.46033Zm-22.77587,96.3164A7.99983,7.99983,0,1,0,27.544,154.91736,103.97631,103.97631,0,0,0,54.45508,201.556a7.99984,7.99984,0,0,0,11.30273-11.32422A87.96258,87.96258,0,0,1,42.99854,150.77673Zm107.7749,62.24219a87.94957,87.94957,0,0,1-45.54981-.01758,8.00007,8.00007,0,1,0-4.14062,15.45508,103.98087,103.98087,0,0,0,53.8457.01367,8.00008,8.00008,0,0,0-4.15527-15.45117Zm72.03418-67.89746a7.99909,7.99909,0,0,0-9.79248,5.666,87.951,87.951,0,0,1-22.78955,39.4375,8.00018,8.00018,0,0,0,11.31347,11.31445,103.97037,103.97037,0,0,0,26.93457-46.625A8.00058,8.00058,0,0,0,222.80762,145.12146ZM213.001,105.224a8.00006,8.00006,0,1,0,15.45507-4.14063A103.9608,103.9608,0,0,0,201.54492,54.4447a7.99984,7.99984,0,0,0-11.30273,11.32422A87.94691,87.94691,0,0,1,213.001,105.224ZM105.22656,42.98084a87.94957,87.94957,0,0,1,45.54981.01757A8.00006,8.00006,0,1,0,154.917,27.54334a104.009,104.009,0,0,0-53.8457-.01368,8.00008,8.00008,0,0,0,4.15527,15.45118Z"></path>{" "}
+              </g>
+            </svg>
+          </div>
+          <img src="public/message.png" className="w-5 h-5 mr-3.5" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#999888"
+          >
+            <path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z" />
+          </svg>
+        </div>
+      </div>
+      <div className="bg-[#9DE1FE] h-23 flex items-center p-5 relative">
+        <div className="flex gap-3">
+          <div className="h-11 w-11 rounded-full bg-white flex justify-center items-center">
+            <img src="public/wifi.png" className="h-7 w-7" />
+          </div>
+          <div>
+            <div className="font-normal flex justify-between">
+              No Contacts
+              <button className="cancel">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="#434343"
+                  className="absolute top-2 right-4"
+                >
+                  <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                </svg>
+              </button>
+            </div>
+            <div className="text-[13px] mt-1 text-gray-700">
+              You can import Contacts from Google
+              <span className="underline"> Learn More.</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="h-11.5 bg-white p-2">
+        <div className="bg-[#EDEDED] p-1.5 rounded-lg flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="rotate-355"
+            height="20px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="#3CB371"
+          >
+            <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+          </svg>
+          <input
+            className="flex-1 border-none outline-none text-[13px] ml-7 text-[#3e3e3e]"
+            placeholder="Search or start a new chat"
+          />
+        </div>
+      </div>
+      <div>
+        <div className="p-3 flex items-center gap-3 bg-[#EDEDED]">
+          <img
+            className="w-12 h-12 rounded-full object-cover"
+            src="public/image.png"
+          />
+          <div>
+            <p className="font-semibold">John Smith</p>
+            <div className="flex gap-1.5 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="rotate-355"
+                height="16px"
+                viewBox="0 -960 960 960"
+                width="16px"
+                fill="#686767"
+              >
+                <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+              </svg>
+              <p className="text-[13px] text-[#686767]">Testing</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border-b border-gray-200">
+        <div className="p-3 flex items-center gap-3 bg-white">
+          <img
+            className="w-12 h-12 rounded-full object-cover"
+            src="public/child.png"
+          />
+          <div>
+            <p className="font-semibold">Jane Doe</p>
+            <div className="flex gap-1.5 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="rotate-355"
+                height="16px"
+                viewBox="0 -960 960 960"
+                width="16px"
+                fill="#686767"
+              >
+                <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+              </svg>
+              <p className="text-[13px] text-[#686767]">Hello there!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border-b border-gray-200">
+        <div className="p-3 flex items-center gap-3 bg-white">
+          <img
+            className="w-12 h-12 rounded-full object-cover"
+            src="public/sitting.png"
+          />
+          <div>
+            <p className="font-semibold">Bob Johnson</p>
+            <div className="flex gap-1.5 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="rotate-355"
+                height="18px"
+                viewBox="0 -960 960 960"
+                width="18px"
+                fill="#1E90FF"
+              >
+                <path d="M268-240 42-466l57-56 170 170 56 56-57 56Zm226 0L268-466l56-57 170 170 368-368 56 57-424 424Zm0-226-57-56 198-198 57 56-198 198Z" />
+              </svg>
+              <p className="text-[13px] text-[#686767]">How are you?</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border-b border-gray-200">
+        <div className="p-3 flex items-center gap-3 bg-white">
+          <img
+            className="w-12 h-12 rounded-full object-cover"
+            src="public/mountain.png"
+          />
+          <div>
+            <p className="font-semibold">Samantha Lee</p>
+            <div className="flex gap-1.5 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="rotate-355"
+                height="16px"
+                viewBox="0 -960 960 960"
+                width="16px"
+                fill="#686767"
+              >
+                <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+              </svg>
+              <p className="text-[13px] text-[#686767]">See you tomorrow!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border-b border-gray-200">
+        <div className="p-3 flex items-center gap-3 bg-white">
+          <img
+            className="w-12 h-12 rounded-full object-cover"
+            src="public/forest.png"
+          />
+          <div>
+            <p className="font-semibold">William Chen</p>
+            <div className="flex gap-1.5 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="18px"
+                viewBox="0 -960 960 960"
+                width="18px"
+                fill="#666666"
+                className="rotate-355"
+              >
+                <path d="M268-240 42-466l57-56 170 170 56 56-57 56Zm226 0L268-466l56-57 170 170 368-368 56 57-424 424Zm0-226-57-56 198-198 57 56-198 198Z" />
+              </svg>
+              <p className="text-[13px] text-[#686767]">
+                Thanks for your help!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border-b border-gray-200">
+        <div className="p-3 flex items-center gap-3 bg-white">
+          <img
+            className="w-12 h-12 rounded-full object-cover"
+            src="public/flowers.png"
+          />
+          <div>
+            <p className="font-semibold">Emily Kim</p>
+            <div className="flex gap-1.5 items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="rotate-355"
+                height="18px"
+                viewBox="0 -960 960 960"
+                width="18px"
+                fill="#1E90FF"
+              >
+                <path d="M268-240 42-466l57-56 170 170 56 56-57 56Zm226 0L268-466l56-57 170 170 368-368 56 57-424 424Zm0-226-57-56 198-198 57 56-198 198Z" />
+              </svg>
+              <p className="text-[13px] text-[#686767]">
+                Are you free tonight?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyComponents;
